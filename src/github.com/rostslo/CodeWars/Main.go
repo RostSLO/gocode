a0b07f0e0ec758d1b200c5a629213d95077f0446
RostSLO <rostbor@gmail.com>
2023-02-23 21:15:04 -0500

    My first code wars test

---
+++ src/github.com/rostslo/CodeWars/Main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func Multiple3And5(number int) int {
+	var sum int = 0
+
+	if number < 3 {
+		return 0
+	}
+
+	for i := 3; i < number; i++ {
+		if i%3 == 0 {
+			sum += i
+		} else if i%5 == 0 {
+			sum += i
+		}
+	}
+
+	return sum
+
+}
+
+func main() {
+
+	res := Multiple3And5(10)
+
+	fmt.Println(res)
+
+}
